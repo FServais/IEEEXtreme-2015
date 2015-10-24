dbb54b1ef58f6eac0235408a642e8940ce37a18b
Fabrice Servais <fabrice.servais@gmail.com>
2015-10-24 02:12:45 +0200

    OK !

--- problem1/main.py
+++ problem1/main.py
@@ -1,1 +1,16 @@
-__author__ = 'Romain'
+
+while True:
+    A0 = input()
+    if A0 == "END": break
+
+    i = 1
+    A_prec = A0
+    A_cur = str(len(A0))
+    while A_prec != A_cur:
+        i += 1
+
+        A_prec, A_cur = A_cur, str(len(A_cur))
+
+        print("A_prec = {}, A_cur = {}".format(A_prec, A_cur))
+
+    print(i)
